93a066202c49abb010353602257cc1e874b847e9
Ale Roses <ale.vrs@outlook.com>
2026-03-24 21:23:28 -0500

    Add styles to the theme and fonts files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+
 function App() {
-  return <div className="app">App</div>;
+  return <div className="app text-red-400">
+    <h1 className="font-dmsans">
+    App
+
+    </h1>
+
+    <h2 className="font-montserrat">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quos pariatur, perferendis quisquam explicabo beatae, ducimus accusantium dicta aspernatur sed ut, sunt aliquam iste? Officia reprehenderit asperiores obcaecati autem voluptate.</h2>
+    </div>;
 }
 
 export default App;
